9d41a89e46db8c06fb3e1a490a70783205bd5e88
Matan Atlas <matanatlas2002@gmail.com>
2025-12-04 13:32:35 -0500

    Initial Commit

---
+++ RunEfficiency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunEfficiency
+//
+//  Created by Matan Atlas on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunEfficiency/RunEfficiencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunEfficiencyApp.swift
+//  RunEfficiency
+//
+//  Created by Matan Atlas on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RunEfficiencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
